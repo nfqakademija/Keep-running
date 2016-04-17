660f7019925d27c5026ead13c76c24dd3b2af93e
Povilas Vaitonis <p.vaitonis@gmail.com>
2016-04-17 12:57:43 +0300

    set drawpath.js

--- src/AppBundle/Resources/scripts/drawpath.js
+++ src/AppBundle/Resources/scripts/drawpath.js
@@ -1,32 +1,43 @@
 /**
  * Created by povilas on 16.4.13.
  */
-    //map DOM pasirinkimas
-var mapDiv = null;
-    //masyvas
-var path = null;
-
-mapDiv = document.getElementById('map');
-path = JSON.parse(document.getElementById('path'));
+//paimti koordinates is json
+var waypoints = JSON.parse(document.getElementById('path'));
 //esama vieta centruoti
 var currentPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
+//---------------------
 function initMap() {
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 6,
-        center: currentPosition,
-        mapTypeId: google.maps.MapTypeId.HYBRID
+        center: currentPosition
     });
-    var line = new google.maps.Polyline({
-        path: [{lat: 22.291, lng: 153.027}, {lat: 18.291, lng: 153.027}],
-        strokeOpacity: 0,
-        icons: [{
-            icon: lineSymbol,
-            offset: '0',
-            repeat: '20px'
-        }],
-        map: map
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+    directionsService.route({
+        origin: currentPosition,
+        destination: currentPosition,
+        waypoints: waypoints,
+        optimizeWaypoints: true,
+        travelMode: google.maps.TravelMode.WALKING
+    }, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+            var route = response.routes[0];
+            var summaryPanel = document.getElementById('directions-panel');
+            summaryPanel.innerHTML = '';
+            // For each route, display summary information.
+            for (var i = 0; i < route.legs.length; i++) {
+                var routeSegment = i + 1;
+                summaryPanel.innerHTML += '<b>Trasa: ' + route.legs[i].distance.text + '<br><br>';
+            }
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
     });
-//galinis map kuri piesiam
-    var generatedMap = new google.maps.Map(mapDiv, options);
 }

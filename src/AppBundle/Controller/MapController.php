f688ffc820295208c871b736959bc368ad573017
Andrius Buivydas <andrius.buivydas@hometogo.com>
2016-04-19 18:45:57 +0300

    using tracks repository

--- src/AppBundle/Controller/MapController.php
+++ src/AppBundle/Controller/MapController.php
@@ -8,11 +8,11 @@ class MapController extends Controller
 {
     public function indexAction()
     {
-        $repository = $this->get('app_bundle.repository');
+        $repository = $this->get('app_bundle.repository.tracks');
         $tracks = $repository->getTracks();
-
+        var_dump($tracks);
         return $this->render('AppBundle:Map:index.html.twig', [
-            'tracks' => $tracks,
+            'tracks' => [],
         ]);
     }
 
--- src/AppBundle/Repository/Tracks.php
+++ src/AppBundle/Repository/Tracks.php
@@ -2,10 +2,28 @@
 
 namespace AppBundle\Repository;
 
+use Doctrine\DBAL\Connection;
+
 class Tracks
 {
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @return array
+     */
     public function getTracks()
     {
-        return [];
-    } 
+        return $this->connection->fetchAll('select * from running_tracks');
+    }
+
+    /**
+     * @param Connection $connection
+     */
+    public function setConnection($connection)
+    {
+        $this->connection = $connection;
+    }
 }

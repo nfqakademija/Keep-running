26b45162fb435c156b17d4fa8931218a1337bcde
Povilas Vaitonis <p.vaitonis@gmail.com>
2016-04-24 13:14:44 +0300

    reading files directory command

---
+++ src/AppBundle/Command/ReadShowCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: povilas
+ * Date: 16.4.24
+ * Time: 12.36
+ */
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ReadShowCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('read:run')
+            ->setDescription('Read and list files')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dir    = './src/AppBundle/Controller';
+        $files= scandir($dir);
+        $output->writeln($files);
+    }
+}

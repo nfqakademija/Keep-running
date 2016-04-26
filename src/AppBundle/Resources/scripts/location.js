f8d9443057a92d00bee63879fd76206704195728
Vaidotas Poskus <vaidotas963@inbox.lt>
2016-04-26 17:01:25 +0300

    Change JS function

--- src/AppBundle/Resources/scripts/location.js
+++ src/AppBundle/Resources/scripts/location.js
@@ -1,24 +1,57 @@
 // var currentPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 var currentPosition = {lat: 54.89692, lng: 23.93794};
 var attr  = document.getElementById('map');
-var directionsDisplay = [];
-var directionsService = [];
 var map = null;
+
 function showRoute() {
    getWayPoints();
 }
 
-function initMap() {
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
-        center:currentPosition/* {lat: 54.89692, lng: 23.93794}*/,
-        mapTypeControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-    directionsDisplay.setMap(map);
+function findPoint(data) {
+    return {
+        'lat': parseFloat(data["lat"]),
+        'lon': parseFloat(data["lon"])
+    };
+}
 
-    return directionsDisplay;
+function findPoints(data) {
+    var pointLength = data.length - 1;
+    var loopStep = Math.floor(pointLength/15);
+    var i = loopStep;
+    var points = [];
+
+    points.push(findPoint(data[0]));
+
+    while(i<pointLength){
+        points.push(findPoint(data[i]));
+        i=i+loopStep;
+    }
+
+    points.push(findPoint(data[data.length - 1]));
+
+    return points;
+}
+
+function groupPoints(wayPoints) {
+    var i = 0;
+    var groups = {
+        'groupA': [],
+        'groupB': []
+    };
+
+    while (i<wayPoints.length){
+        if(i>=0 && i<10){
+            groups.groupA.push(wayPoints[i]);
+            if(i==9){
+                groups.groupB.push(wayPoints[9]);
+            }
+        }else{
+            groups.groupB.push(wayPoints[i]);
+        }
+        i++;
+    }
+
+    return groups;
 }
 
 function getWayPoints() {
@@ -28,70 +61,40 @@ function getWayPoints() {
         mapTypeControl: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var wayPoints = [];
+
     var wayPointsFromAttribute= JSON.parse(attr.getAttribute('data-points'));
     wayPointsFromAttribute = wayPointsFromAttribute.points;
-    var pointLength=wayPointsFromAttribute.length-1;
-    //First rout point
-    var firstRoutePoints = wayPointsFromAttribute[0];
-    //Last rout point
-    var lastRoutePoints = wayPointsFromAttribute[pointLength];
-    //First rout point Latitude and longtitude
-    var firstRoutePointLatitude = parseFloat(firstRoutePoints["lat"]);
-    var firstRoutePointLongtitude = parseFloat(firstRoutePoints["lon"]);
-    wayPoints.push({
-        location: new google.maps.LatLng(firstRoutePointLatitude,firstRoutePointLongtitude),
-        stopover: true
-    });
-    var loopStep = Math.floor(pointLength/15);
-    var i = loopStep;
-    while(i<pointLength){
-        var routeLatitude = parseFloat(wayPointsFromAttribute[i]["lat"]);
-        var routeLongtitude = parseFloat(wayPointsFromAttribute[i]["lon"]);
+
+    var wayPoints = [];
+    var points = findPoints(wayPointsFromAttribute);
+    for (var i = 0; i < points.length; i++) {
         wayPoints.push({
-            location: new google.maps.LatLng(routeLatitude,routeLongtitude),
+            location: new google.maps.LatLng(points[i]['lat'],points[i]['lon']),
             stopover: true
         });
-        i=i+loopStep;
     }
-    //Last rout point Latitude and longtitude
-    var lastRoutePointLatitude = parseFloat(lastRoutePoints["lat"]);
-    var lastRoutePointLongtitude = parseFloat(lastRoutePoints["lon"]);
-    wayPoints.push({
-        location: new google.maps.LatLng(lastRoutePointLatitude,lastRoutePointLongtitude),
-        stopover: true
-    });
+
     map = new google.maps.Map(attr, mapOptions );
     google.maps.event.addDomListener(window,'resize',function() {
         google.maps.event.trigger(map,'resize');
     });
-    var i = 0;
-    var tmpWayPointsArray1=[];
-    var tmpWayPointsArray2=[];
-    while (i<wayPoints.length){
-        if(i>=0 && i<10){
-            tmpWayPointsArray1.push(wayPoints[i]);
-             if(i==9){
-                    tmpWayPointsArray2.push(wayPoints[9]);
-             }
-        }else{
-            tmpWayPointsArray2.push(wayPoints[i]);
-        }
-        i++;
-    }
-    plotTrack(tmpWayPointsArray1);
-    plotTrack(tmpWayPointsArray2);
+
+    var groupedPoints = groupPoints(wayPoints);
+    plotTrack(groupedPoints.groupA);
+    plotTrack(groupedPoints.groupB);
 }
 
 function plotTrack(wayPoints) {
-    var startPoint = wayPoints[0].location
+    var startPoint = wayPoints[0].location;
     var startPointLatitde = startPoint.lat();
     var startPointLongtitude = wayPoints[0].location.lng();
     var endPoint = wayPoints[wayPoints.length-1].location;
     var endPointLatitde = endPoint.lat();
     var endPointLongtitude = endPoint.lng();
+
     wayPoints.shift();
     wayPoints.pop();
+
     var request = {
         origin: new google.maps.LatLng(startPointLatitde,startPointLongtitude),
         destination: new google.maps.LatLng(endPointLatitde,endPointLongtitude),
@@ -99,11 +102,16 @@ function plotTrack(wayPoints) {
         optimizeWaypoints: true,
         travelMode: google.maps.TravelMode.WALKING
     };
+
+    var directionsDisplay = [];
+    var directionsService = [];
+
     directionsService.push(new google.maps.DirectionsService());
-    var instance = directionsService.length - 1;
     directionsDisplay.push(new google.maps.DirectionsRenderer({
         preserveViewport: true
     }));
+
+    var instance = directionsService.length - 1;
     directionsDisplay[instance].setMap(map);
     directionsService[instance].route(request, function (response, status) {
         if (status == google.maps.DirectionsStatus.OK) {

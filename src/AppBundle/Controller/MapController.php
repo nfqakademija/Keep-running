593e4c91f7600712a29d16ce43996da6a6e89c27
Vaidotas Poskus <vaidotas963@inbox.lt>
2016-05-05 20:07:07 +0300

    Find track by id

--- src/AppBundle/Controller/MapController.php
+++ src/AppBundle/Controller/MapController.php
@@ -14,32 +14,37 @@ class MapController extends Controller
         $trackLevels = $repositoryLevels->getLevels();
         $maxDistance = $repositoryTracks->getMaxDistance();
         $maxDistanceToKilometer = ceil($maxDistance[0]['MAX(`running_tracks`.`trackDistance`)'] / 1000);
-        $distance = [];
-        $distanceFrom = $request->query->get('distance_from') ?: null;
-        $distanceTo = $request->query->get('distance_to') ?: null;
-        $distanceFromKm = (integer)$distanceFrom * 1000;
-        $distanceToKm = (integer)$distanceTo * 1000;
-        $distance = [
-            'distanceFrom' => $distanceFromKm,
-            'distanceTo' => $distanceToKm
-        ];
+        $trackId = $request->query->get('trackId') ?: null;
+        $distanceFrom = $request->query->get('distance_from') ?: 2;
+        $distanceTo = $request->query->get('distance_to') ?: $maxDistanceToKilometer;
         $difficulty = (integer)$request->query->get('difficulty') ?: null;
-        $repository = $this->get('app_bundle.repository.tracks');
-        $tracksAfterFilter = $repository->getTracksByFilter($distance, $difficulty);
-        $countTracksAfterFilter = count($tracksAfterFilter) - 1;
-        $randomTrackNumber = rand(0, $countTracksAfterFilter);
-        $trackId = $tracksAfterFilter[$randomTrackNumber]['trackId'];
-        $track = $repository->getTrackById($trackId);
+        if($trackId){
+            $track = $repositoryTracks->getTrackById($trackId);
+        }else{
+            $distance = [];
+            $distanceFromKm = (integer)$distanceFrom * 1000;
+            $distanceToKm = (integer)$distanceTo * 1000;
+            $distance = [
+                'distanceFrom' => $distanceFromKm,
+                'distanceTo' => $distanceToKm
+            ];
+            $tracksAfterFilter = $repositoryTracks->getTracksByFilter($distance, $difficulty);
+            $countTracksAfterFilter = count($tracksAfterFilter) - 1;
+            $randomTrackNumber = rand(0, $countTracksAfterFilter);
+            $trackId = $tracksAfterFilter[$randomTrackNumber]['trackId'];
+            $track = $repositoryTracks->getTrackById($trackId);
+        }
         $points = $track[0]['trackPoints'];
-        $trackInformation = $repository->getInformationAboutTrack($trackId)[0];
-        $request->query->add(['trackId' => $trackId]);
+        $trackInformation = $repositoryTracks->getInformationAboutTrack($trackId)[0];
+        $trackUrl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getPathInfo().'?trackId='.$trackId;
         return $this->render('AppBundle:Map:index.html.twig', [
             'points' => $points,
             'tracks_levels' => $trackLevels,
             'max_distance' => $maxDistanceToKilometer,
             'track_info' => $trackInformation,
             'distance_from' => $distanceFrom,
-            'distance_to' => $distanceTo
+            'distance_to' => $distanceTo,
+            'track_url' => $trackUrl
         ]);
     }
 }
